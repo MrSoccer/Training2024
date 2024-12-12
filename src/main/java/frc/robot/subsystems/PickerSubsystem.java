16cd69fe85c025e1f13e9316e2abd0e6a0e088ff
Darin Wells <dwells@amalsugar.com>
2024-12-11 20:51:04 -0700

    init repo with picker, shooter, drive subsystems

---
+++ src/main/java/frc/robot/subsystems/PickerSubsystem.java
@@ -0,0 +1,68 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class PickerSubsystem extends SubsystemBase {
+    private CANSparkMax oneMotor;
+    private DigitalInput oneSensor;
+
+    private FlywheelSim oneMotorSim;
+
+    public enum PickerStates {
+        PICKING,
+        IDLING,
+        EJECTING
+    }
+    private PickerStates newState = PickerStates.IDLING;
+
+    public PickerSubsystem() {
+        oneMotor = new CANSparkMax(15, MotorType.kBrushless);
+        oneSensor = new DigitalInput(7);
+        oneMotorSim = new FlywheelSim(
+            DCMotor.getNeo550(1), 
+            1, 
+            .0001
+        );
+    }
+
+    @Override
+    public void periodic() {
+        switch (newState) {
+            case PICKING -> {
+                oneMotor.setVoltage(12);
+                oneMotorSim.setInput(12);
+                if (!oneSensor.get()) {
+                    newState = PickerStates.IDLING;
+                }
+            }
+            case IDLING -> {
+                oneMotorSim.setInput(0);
+                oneMotor.setVoltage(0);
+            }
+            case EJECTING -> {
+                oneMotorSim.setInput(-12);
+                oneMotor.setVoltage(-12);
+            }
+        }
+
+        SmartDashboard.putNumber("motor speed", oneMotor.get());
+        SmartDashboard.putString("picker state", newState.toString());
+    }
+
+    @Override
+    public void simulationPeriodic() {
+        oneMotorSim.update(.02);
+        SmartDashboard.putNumber("Picker/Speed", oneMotorSim.getAngularVelocityRPM());
+    }
+
+    public void setState(PickerStates state) {
+        newState = state;
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,54 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+    public enum ShooterStates {
+        IDLING,
+        SHOOTING,
+        EJECTING
+    }
+    private ShooterStates state = ShooterStates.IDLING; 
+
+    private CANSparkMax motor;
+    private FlywheelSim motorSim;
+
+    private final int MOTOR_ID = 20;
+
+    public ShooterSubsystem() {
+        motor = new CANSparkMax(MOTOR_ID, MotorType.kBrushless);
+        motorSim = new FlywheelSim(
+            DCMotor.getNeo550(1),
+            1,
+            .001
+        );
+    }
+
+    @Override
+    public void periodic() {
+        switch (state) {
+            case IDLING -> {
+                motor.setVoltage(0);
+            }
+            case SHOOTING -> {
+                motor.setVoltage(12);
+            }
+            case EJECTING -> {
+                motor.setVoltage(-12);
+            }
+        }
+
+        SmartDashboard.putNumber("shooter motor speed", motor.get());
+        SmartDashboard.putString("shooter state", state.toString());
+    }
+
+    public void setState(ShooterStates state) {
+        this.state = state;
+    }
+}
